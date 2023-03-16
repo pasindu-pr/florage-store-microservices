66796cf78e35818b2e696096e22ee317cf3f6f49
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-03-16 21:52:02 +0530

    feat: remove mssql and add mongob and auth endpoints with asp identity

---
+++ src/services/Florage.Authentication/Contracts/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Florage.Authentication.Dtos;
+using Florage.Authentication.Responses;
+using Microsoft.AspNetCore.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Florage.Authentication.Contracts
+{
+    public interface IUserService
+    {
+        public Task<IdentityResult> RegisterAsync(UserRegisterDto userRegisterDto);
+        public Task<UserTokenResponse> LoginAsync(UserLoginDto userLoginDto);
+    }
+}
---
+++ src/services/Florage.Authentication/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Florage.Authentication.Contracts;
+using Florage.Authentication.Dtos;
+using Florage.Authentication.Responses;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Florage.Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        } 
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult<IdentityResult>> CreateUser(UserRegisterDto user)
+        {
+            IdentityResult identityResult = await _userService.RegisterAsync(user);
+            
+            if (identityResult.Succeeded)
+            {
+                return Ok(identityResult);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<UserTokenResponse>> Login(UserLoginDto user)
+        {
+            UserTokenResponse tokenResponse = await _userService.LoginAsync(user);
+
+            if (tokenResponse.Success)
+            {
+                return Ok(tokenResponse);
+            }
+
+            return BadRequest(tokenResponse);
+        }
+    }
+}
