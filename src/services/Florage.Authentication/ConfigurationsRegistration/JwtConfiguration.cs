4db46d1dfd9fe4ea06b6c47b3e175431d716a14c
SDSamarasinghe <pasinduprabashitha@gmail.com>
2023-03-16 22:09:36 +0530

    feat: refactor implementation

---
+++ src/services/Florage.Authentication/ConfigurationsRegistration/JwtConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Florage.Authentication.Configurations
+{
+    public static class JwtConfiguration
+    {
+        public static IServiceCollection JwtConfigurationRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            // Adding Jwt Bearer
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = configuration["JWT:ValidAudience"],
+                    ValidIssuer = configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+                };
+            });
+            
+            return services;
+        }
+    }
+}
---
+++ src/services/Florage.Authentication/ConfigurationsRegistration/RegisterIdentity.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCore.Identity.Mongo;
+using AspNetCore.Identity.Mongo.Model;
+
+namespace Florage.Authentication.ConfigurationsRegistration
+{
+    public static class RegisterIdentity
+    {
+        public static IServiceCollection ConfigureIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentityMongoDbProvider<MongoUser, MongoRole>(identity =>
+            {
+                identity.Password.RequiredLength = 8;
+            },
+                mongo =>
+            {
+                mongo.ConnectionString = configuration.GetConnectionString("DefaultConnection");
+            });
+
+            return services;
+        }
+    }
+}
