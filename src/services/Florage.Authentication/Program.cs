32658c709e7ec4a13bc35c763cc07df45de6761c
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-03-16 22:09:36 +0530

    feat: refactor implementation

--- src/services/Florage.Authentication/Program.cs
+++ src/services/Florage.Authentication/Program.cs
@@ -1,60 +1,20 @@
-using AspNetCore.Identity.Mongo;
-using AspNetCore.Identity.Mongo.Model;
-using Florage.Authentication.Contracts; 
+using Florage.Authentication.Configurations;
+using Florage.Authentication.ConfigurationsRegistration;
+using Florage.Authentication.Contracts;
 using Florage.Authentication.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); 
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers(); 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddSwaggerGen();  
 builder.Services.AddScoped<IUserService, UserService>();
 
-builder.Services.AddIdentityMongoDbProvider<MongoUser, MongoRole>(identity =>
-{
-    identity.Password.RequiredLength = 8;
-    // other options
-},
-    mongo =>
-    {
-        mongo.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-        // other options
-    });
-
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-
-// Adding Jwt Bearer
-.AddJwtBearer(options =>
-{
-    options.SaveToken = true;
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = builder.Configuration["JWT:ValidAudience"],
-        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
-    };
-});
+JwtConfiguration.JwtConfigurationRegistration(builder.Services, builder.Configuration);
+RegisterIdentity.ConfigureIdentityServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
