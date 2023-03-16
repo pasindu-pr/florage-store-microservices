d303ebdb4e61a86562d7fde84afc29bd3a7f768c
SDSamarasinghe <pasinduprabashitha@gmail.com>
2023-03-16 16:58:09 +0530

    feat: add asp.net identity

---
+++ src/services/Florage.Authentication/Data/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Florage.Authentication.Data
+{
+    public class DbContext: IdentityUserContext<IdentityUser>
+    {
+        public DbContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
