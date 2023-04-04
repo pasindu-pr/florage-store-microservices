085d857020695d65d6ba8d00382fe5493887ae1b
SDSamarasinghe <pasinduprabashitha@gmail.com>
2023-04-04 21:58:58 +0530

    feat✨: add db configs to shared project

---
+++ src/services/Florage.Shared/Settings/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Florage.Shared.Settings
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+    }
+}
