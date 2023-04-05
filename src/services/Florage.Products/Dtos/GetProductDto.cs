877c1419740d7ae0bd0f5a62d8f77a781a7f58e5
SDSamarasinghe <pasinduprabashitha@gmail.com>
2023-04-05 08:05:35 +0530

    feat ✨: add create and read operations

---
+++ src/services/Florage.Products/Dtos/GetProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Florage.Products.Dtos
+{
+    public class GetProductDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public int StockCount { get; set; }
+    }
+}
