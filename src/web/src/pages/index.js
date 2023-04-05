8f3358fbbde531af55ede1b8154db06073e3c90d
lksad <it20605902@my.sliit.lk>
2023-04-05 17:29:52 +0530

    add some features to product.jsx page

--- src/web/src/pages/index.js
+++ src/web/src/pages/index.js
@@ -1,7 +1,9 @@
 import Homepage from "./homepage";
 import Account from "./account";
 import Login from "./login";
+import Product from "./product";
 
 export { Homepage };
 export {Account};
 export {Login};
+export {Product};
