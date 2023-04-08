debdf3685eb05e249a8581eeb86754254bda48a6
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-04-08 19:54:38 +0530

    feat: add set collection method to interface

--- src/services/Florage.Shared/Contracts/IRepository.cs
+++ src/services/Florage.Shared/Contracts/IRepository.cs
@@ -11,5 +11,6 @@ namespace Florage.Shared.Contracts
         Task<T> FilterAsync(Expression<Func<T, bool>> filter);
         Task<T> GetByIdAsync(string id); 
         Task UpdateAsync(string id, T entity);
+        public void SetCollectionName(string collectionName);
     }
 }
