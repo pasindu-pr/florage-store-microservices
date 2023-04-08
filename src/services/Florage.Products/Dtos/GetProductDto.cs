2fd62cc6a4ed17fba163ac5d8db72e95c933c2d4
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-04-08 21:55:32 +0530

    feat: sync logics with shared pacakge

--- src/services/Florage.Products/Dtos/GetProductDto.cs
+++ src/services/Florage.Products/Dtos/GetProductDto.cs
@@ -2,7 +2,7 @@
 {
     public class GetProductDto
     {
-        public string ProductId { get; set; } = string.Empty;
+        public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Image { get; set; } = string.Empty;
--- src/services/Florage.Products/Models/Product.cs
+++ src/services/Florage.Products/Models/Product.cs
@@ -3,8 +3,7 @@
 namespace Florage.Products.Models
 {
     public class Product: BaseEntity
-    {
-        public string ProductId { get; set; } = string.Empty;
+    { 
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Image { get; set; } = string.Empty;
