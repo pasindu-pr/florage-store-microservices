1f95a2ca5de044b445625242fedf1b815bddf490
SDSamarasinghe <pasinduprabashitha@gmail.com>
2023-04-09 18:34:26 +0530

    feat ✨: add ud operations on products and order payment state change logic

--- src/services/Florage.Orders/Dtos/GetOrderDto.cs
+++ src/services/Florage.Orders/Dtos/GetOrderDto.cs
@@ -4,6 +4,7 @@ namespace Florage.Orders.Dtos
 {
     public class GetOrderDto
     {
+        public string Id { get; set; } = string.Empty;
         public List<OrderProduct>  Products { get; set; } = new List<OrderProduct>();
         public string? UserId { get; set; }
         public float TotalPrice { get; set; }
