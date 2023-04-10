259cadeb8bcfb52c00b00b8750ddb9eeb24fe5ea
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-04-10 20:49:43 +0530

    feat ✨: add inventory service

---
+++ src/services/Florage.Inventory/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using Florage.Inventory.Contracts;
+using Florage.Inventory.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Florage.Inventory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyCollection<GetProductDto>>> GetAllProductsAsync()
+        {
+            IReadOnlyCollection<GetProductDto> products = await _productService.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult<GetProductDto>> GetProductByIdAsync(string id)
+        {
+            GetProductDto product = await _productService.GetByIdAsync(id);
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductAsync(CreateProductDto productDto)
+        {
+            GetProductDto insertedProduct = await _productService.CreateAsync(productDto);
+            return new ObjectResult(insertedProduct) { StatusCode = StatusCodes.Status201Created };
+        }
+
+        [HttpPut("id")]
+        public async Task<IActionResult> UpdateProductAsync(string id, UpdateProductDto updateProductDto)
+        {
+            await _productService.UpdateAsync(id, updateProductDto);
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteProductAsync(string id)
+        {
+            await _productService.DeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
