3b46849b3310dacfe7a48d3af51088c720472774
SDSamarasinghe <pasinduprabashitha@gmail.com>
2023-04-10 20:49:43 +0530

    feat ✨: add inventory service

---
+++ src/services/Florage.Inventory/Profiles/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Florage.Inventory.Dtos;
+using Florage.Inventory.Models;
+
+namespace Florage.Inventory.Profiles
+{
+    public class MappingProfiles: Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, GetProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+        }
+    }
+}
---
+++ src/services/Florage.Inventory/Services/ProductsService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Florage.Inventory.Contracts;
+using Florage.Inventory.Dtos;
+using Florage.Inventory.Models;
+using Florage.Shared.Contracts;
+
+namespace Florage.Inventory.Services
+{
+    public class ProductsService : IProductService
+    {
+        private readonly IRepository<Product> _repository;
+        private readonly IMapper _mapper; 
+
+        public ProductsService(IRepository<Product> genericRepository, IMapper mapper)
+        {
+            _repository = genericRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetProductDto> CreateAsync(CreateProductDto productDto)
+        {
+            Product product = _mapper.Map<Product>(productDto); 
+            Product insertedProduct =  await _repository.CreateAsync(product);
+            GetProductDto insertedMappedProduct = _mapper.Map<GetProductDto>(insertedProduct);
+            return insertedMappedProduct;
+        }
+
+        public async Task DeleteAsync(string productId)
+        {
+            await _repository.DeleteAsync(productId);
+        }
+
+        public async Task<IReadOnlyCollection<GetProductDto>> GetAllAsync()
+        {
+            IReadOnlyCollection<Product> products = await _repository.GetAllAsync();
+            IReadOnlyCollection<GetProductDto> mappedProducts = _mapper.Map<IReadOnlyCollection<GetProductDto>>(products);
+            return mappedProducts;
+        }
+
+        public async Task<GetProductDto> GetByIdAsync(string id)
+        {
+            // Get product by custom product id
+            Product product = await _repository.GetByIdAsync(id);
+            GetProductDto mappedProduct = _mapper.Map<GetProductDto>(product);
+            return mappedProduct;
+        }
+
+        public async Task UpdateAsync(string productId,UpdateProductDto updateProductDto)
+        {
+            Product productToUpdate = _mapper.Map<Product>(updateProductDto); 
+            
+            productToUpdate.Id = productId; 
+            await _repository.UpdateAsync(productId, productToUpdate);
+        }
+    }
+}
