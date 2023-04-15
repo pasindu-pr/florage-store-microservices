51e2838f4bf94b54dd4644451c509e70d6bfdbd5
SDSamarasinghe <pasinduprabashitha@gmail.com>
2023-04-15 08:06:42 +0530

    feat :sparkles: : add rabbit mq config to shared package

---
+++ src/services/Florage.Shared/Configurations/AsyncMessagingConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using Florage.Shared.Contracts;
+using Florage.Shared.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Florage.Shared.Configurations
+{
+    public static class AsyncMessagingConfigurations
+    {
+        public static IServiceCollection AddRabbitMq(this IServiceCollection services)
+        {
+            services.AddSingleton<IRabbitMqService, RabbitMqService>();
+            return services;
+        }
+    }
+}
