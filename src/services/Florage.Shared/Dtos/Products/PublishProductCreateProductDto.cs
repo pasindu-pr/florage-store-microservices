d911526306de9633de18a4359f429483598e8676
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-04-18 16:57:37 +0530

    feat :sparkles: : Add product related messaging dtos to shared package

---
+++ src/services/Florage.Shared/Dtos/Products/PublishProductCreateProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Florage.Shared.Dtos.Products
+{
+    public class PublishProductCreateProductDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public int StockCount { get; set; }
+    }
+}
