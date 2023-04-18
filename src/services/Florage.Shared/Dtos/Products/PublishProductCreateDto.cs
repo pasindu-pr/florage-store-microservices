971462a99e941713a8db52ed5733037b9a410cc5
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-04-18 18:24:59 +0530

    feat :sparkles: : add missing attributes to the product publish dto

--- src/services/Florage.Shared/Dtos/Products/PublishProductCreateDto.cs
+++ src/services/Florage.Shared/Dtos/Products/PublishProductCreateDto.cs
@@ -2,6 +2,7 @@
 {
     public class PublishProductCreateDto
     {
+        public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Image { get; set; } = string.Empty;
