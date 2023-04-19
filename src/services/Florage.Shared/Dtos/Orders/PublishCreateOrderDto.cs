30bc39a1a9721bb8bed2947c065e5a4e398f7bcc
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-04-19 21:11:06 +0530

    feat :sparkles: : add publish create order dto

---
+++ src/services/Florage.Shared/Dtos/Orders/PublishCreateOrderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Florage.Shared.Dtos.Orders
+{
+    internal class PublishCreateOrderDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string? UserId { get; set; }
+        public float TotalPrice { get; set; }
+    }
+}
