7cb3ecc71c655945a68eccf0db936f987bda10fa
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-04-19 13:56:46 +0530

    feat :sparkles: : add prefix to controller

--- src/services/Florage.Inventory/Controllers/ProductsController.cs
+++ src/services/Florage.Inventory/Controllers/ProductsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Florage.Inventory.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/inventory/products")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
