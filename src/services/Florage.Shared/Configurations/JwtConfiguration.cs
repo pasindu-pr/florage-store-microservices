df563b2b52e1ab9e12bce3d37846185c2e278868
SDSamarasinghe <pasinduprabashitha@gmail.com>
2023-04-22 08:13:55 +0530

    refactor: :recycle: : move auth configurations to shared project

---
+++ src/services/Florage.Shared/Configurations/JwtConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Florage.Shared.Configurations
+{
+    public static class JwtConfiguration
+    {
+        public static IServiceCollection AddJwtAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = configuration["JWT:ValidAudience"],
+                    ValidIssuer = configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+                };
+            });
+
+            return services;
+        }
+    }
+}
