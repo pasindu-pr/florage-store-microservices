474bfd86045cb13749f7b708cc1682baf9a6c293
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-04-22 08:13:55 +0530

    refactor: :recycle: : move auth configurations to shared project

---
+++ src/services/Florage.Shared/Configurations/SwaggerAuthorizationConfigurations.cs
@@ -0,0 +1,45 @@
+﻿using Florage.Shared.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Florage.Shared.Configurations
+{
+    public static class SwaggerAuthorizationConfigurations
+    {
+        public static IServiceCollection AddSwaggerAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            ServiceSettings? serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+            services.AddSwaggerGen(option =>
+                {
+                    option.SwaggerDoc("v1", new OpenApiInfo { Title = $"Florage {serviceSettings.ServiceName} API", Version = "v1" });
+                    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                    });
+                    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    new string[]{}
+                    }                   
+                    });
+                });
+
+            return services;
+        }
+    }
+}
