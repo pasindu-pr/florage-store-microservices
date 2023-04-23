507c98121af4def2647a3b82418a02fc73418fd9
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-04-23 13:26:34 +0530

    feat :sparkles: : add publish order confirmed dto

---
+++ src/services/Florage.Shared/Dtos/Notifications/PublishOrderConfirmedDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Florage.Shared.Dtos.Notifications
+{
+    public class PublishOrderConfirmedDto
+    {
+        public string OrderId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPhoneNumber { get; set; } = string.Empty;
+        public float Price { get; set; }
+    }
+}
