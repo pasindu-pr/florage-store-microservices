bcbde37eca51cd000d3428197d0be6984a8e8538
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-04-26 08:11:46 +0530

    feat :sparkles: : add payment created dto

---
+++ src/services/Florage.Shared/Dtos/Payment/PublishPaymentCreatedDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Florage.Shared.Dtos.Payment
+{
+    public class PublishPaymentCreatedDto
+    {
+        public string OrderId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
